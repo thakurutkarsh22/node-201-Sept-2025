7ce1e67f81781b58cd407dca27ec281e591aa65e
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2025-09-05 21:18:03 +0530

    session 3

--- Session3/index.js
+++ Session3/index.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const userData = require("./userData");
+const UserActivationRouter = require("./Routes/UserActivityRoute");
+const HomeRouter = require("./Routes/HomeRoute");
+const { homeResponse, aboutResponse } = require("./Controllers/HomeController");
+const { getAllUsers, getUserByGender, getUserByFirstName } = require("./Controllers/UserActivityController");
 const server = express();
 const PORT = 8089;
 
-server.get("/", (req, res) => {
-  res.status(200).send("Welcome to my fitness pagge Express JS ");
-});
 
-server.get("/abouts", (req, res) => {
-  res
-    .status(200)
-    .send("About Us: We are a company that does fitness CRM, DataRAG");
-});
+server.use("/", HomeRouter);
 
 server.get("/fitness", (req, res) => {
   const fitnessData = {
@@ -23,44 +20,22 @@ server.get("/fitness", (req, res) => {
     shouldSleep8Hours: true,
   };
 
-  // json is a method in express to send json response
-  //   json is setting the content type as application/json automatically
   res.status(200).json(fitnessData);
 });
 
-// 1. get all users
-server.get("/api/v1/users", (req, res) => {
-  res.status(200).json(userData);
-});
-
-// 2 get user by gender
-// way 1 query params (? in the URL)
-server.get("/api/v1/users/search", (req, res) => {
-  const params = req.query;
-  const searchedGender = params.gender;
-
-  const filteredUsers = userData.data.filter(
-    (user) => user.gender === searchedGender
-  );
-  res.status(200).json({ data: filteredUsers, size: filteredUsers.length });
-});
-
-// 3 get user by id
-// way 2 params (after : in the url path will be a param)
-server.get("/api/v1/users/:firstName", (req, res) => {
-  const params = req.params;
-  const searchedFirstName = params.firstName;
-
-  const user = userData.data.find(
-    (user) => user.name.first === searchedFirstName
-  );
-  if (user) {
-    res.status(200).json({ data: user });
-  } else {
-    res.status(404).json({ message: "User not found" });
-  }
-});
+// ACTIVITY ROUTEs
+// server.use supports all the GET, POST, PUT, DELETE
+server.use("/api/v1/users", UserActivationRouter);
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+// nodejs 
+// common js -> require(' ') -> to import 
+// common js -> module.exports = { } -> to export
+
+// reactjs, js, angular, vuejs 
+// es6 -> export { } -> to export
+// es6 -> import { } from ' ' -> to import
